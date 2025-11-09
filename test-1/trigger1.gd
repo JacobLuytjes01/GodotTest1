24d9567496ff66d4d600de45900c75f4b8e1fab3
Jacob Luytjes <jluytjes01@aurora.edu>
2025-11-09 17:04:16 -0600

    Delete test-1 directory

--- test-1/trigger1.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-func test1_on_body_entered(body):
-	if body.has_method("turnGreen"):
-		body.turnGreen()
-		
-func test1_on_body_exited(body):
-	if body.has_method("turnBack"):
-		body.turnBack()
